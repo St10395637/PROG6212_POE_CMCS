019ea31392412bc92947166491b22f90de507bef
Kgau <166824885+St10395637@users.noreply.github.com>
2024-11-25 15:59:27 +0200

    Update DbContext.cs
    Add comments to the code for better understanding

--- Models/DbContext.cs
+++ Models/DbContext.cs
@@ -1,21 +1,27 @@
-﻿using Claiming_System.Models;
+﻿// Using statements for necessary namespaces
+using Claiming_System.Models;
 using Microsoft.EntityFrameworkCore;
 using MySql.Data.MySqlClient;
 
 public class ApplicationDbContext : DbContext
 {
+    // Constructor that passes the DbContextOptions to the base class constructor
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
 
-    // DbSets for your tables
-   
+    // DbSets represent collections of entities (tables) in the database
     
-
+    // Represents the 'Users' table in the database
     public DbSet<User> Users { get; set; }
+    
+    // Represents the 'Lecturers' table in the database
     public DbSet<Lecturer> Lecturers { get; set; }
+    
+    // Represents the 'Claims' table in the database
     public DbSet<Claim> Claims { get; set; }
+    
+    // Represents the 'SupportingDocuments' table in the database
     public DbSet<SupportingDocuments> SupportingDocuments { get; set; }
 }
-
