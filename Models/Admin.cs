b217f770702bdce4f7b1ec8b7e49fd5952de730f
Kgau <166824885+St10395637@users.noreply.github.com>
2024-11-25 16:52:25 +0200

    Update Admin.cs
    Added comments to the code

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,9 +1,16 @@
 ﻿namespace Claiming_System.Models
 {
+    // The Admin class represents an administrator in the claiming system
     public class Admin
     {
+        // Property to store the name of the admin
         public string Name { get; set; }
+
+        // Property to store the email of the admin
         public string Email { get; set; }
+
+        // Property to store the password of the admin
+        // In production, passwords should be hashed and not stored as plain text
         public string Password { get; set; }
     }
 }
